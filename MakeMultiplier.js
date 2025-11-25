e1a50441289041f97b547a0c67b0f1700899deba
tamanna <tamannabeniwal007@gmail.com>
2025-11-25 16:26:25 +0530

    add make multiplier

---
+++ MakeMultiplier.js
@@ -0,0 +1,23 @@
+function Person(name) {
+    this.name = name;
+}
+
+Person.prototype.showName = function () {
+    console.log("Name:", this.name);
+};
+
+function Student(name, branch) {
+    Person.call(this, name);   
+    this.branch = branch;
+}
+
+Student.prototype = Object.create(Person.prototype);
+
+Student.prototype.showBranch = function () {
+    console.log("Branch:", this.branch);
+};
+
+const s1 = new Student("Bhavya", "Computer Science");
+
+s1.showName();    
+s1.showBranch();  
